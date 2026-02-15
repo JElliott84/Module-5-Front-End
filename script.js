ad1523b3106f09eac7c464c2d33d8705f6b0c479
Jason Elliott <jasonelliott1984@outlook.com>
2026-02-15 14:55:10 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//we are going to make an event listner for when the DOM is loaded
+addEventListener("DOMContentLoaded", async function(){
+    const response = await this.fetch("http://localhost:3000/api/songs")
+    const songs = await response.json()
+
+    let html=""
+    for (let song of songs){
+        html+= <li>${song.title} -${song.artist}</li>
+    }
+
+    document.querySelector("#addedsong").innerHTML = html
+})
